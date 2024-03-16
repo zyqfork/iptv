b8daf012f969dde44530179cdf09b3f76112b2de
zouyq <zyqcn@live.com>
2024-03-16 15:55:06 +0800

    bbb

--- script/iptv.py
+++ script/iptv.py
@@ -87,8 +87,9 @@ with open(output_file, 'w', encoding='utf-8') as file:
         modified_title = re.sub(r'(超清|高清)', '', results[key])
         file.write(modified_title + '\n')
     with open("./script/iptv.txt", 'r', encoding='utf-8') as iptv_miss:
-        iptv_miss_list = iptv_miss.readlines()
-        file.writelines(iptv_miss_list)
+        for line in iptv_miss:
+            modified_line = "#EXTINF:-1 group-title=\"IPTV\", " + line.strip().replace(',', '\n') + '\n'
+            file.write(modified_line)
     
 
 with open("./IPTV.m3u", 'r', encoding='utf-8') as iptv_file:
